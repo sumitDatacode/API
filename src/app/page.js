95c6d9504498573714de7d6b0b354306c572d7ce
SumitSahu444 <sumitkumarsahu141@gmail.com>
2025-04-02 22:04:21 +0530

    show user data on frontend

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,15 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 const page = () => {
-
+const [data, setData] = useState([])
 
   const getaAPIData = async() =>
   {
-          const response = await axios.get('https://randomuser.me/api/')
 
-          console.log(response.data.results)
+          const response = await axios.get('https://randomuser.me/api/')
+          { console.log(response.data.results) }
+          setData(response.data.results)
   }
   useEffect(()=>{
     getaAPIData()
@@ -17,7 +18,18 @@ const page = () => {
 
   return (
     <div>
-       hi
+        {
+          data.map((value, index)=>(
+         
+            <div key={index}>
+              {/* <h1>Value {JSON.stringify(value)}</h1> */}
+            <h1>Name: {value.name.first} {value.name.last}</h1>
+            <p>Email: {value.email}</p>
+            <img src={value.picture.large} alt="User img" />
+          </div>
+          ))
+         
+        }
       
     </div>
   )
